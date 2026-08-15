0060aea8303e6761d848b87b4f4dea195010289a
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-08-15 19:00:58 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/senai/sistema_almoxarifado/SistemaAlmoxarifadoApplication.java
@@ -0,0 +1,13 @@
+package com.senai.sistema_almoxarifado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAlmoxarifadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAlmoxarifadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/sistema_almoxarifado/SistemaAlmoxarifadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.sistema_almoxarifado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAlmoxarifadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
